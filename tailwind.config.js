4368376b0c5469a00e5b7a5a138b831019dbf1ff
Ankan <ankantalukdar88@gmail.com>
2023-12-18 21:31:59 +0530

    Too many bugs— I don't know if I'll fix this in the future.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        priamry_blue: {
+          100: 'hsl(243, 100%, 93%)',
+          200: 'hsl(229, 7%, 55%)',
+          300: 'hsl(228, 56%, 26%)',
+          400: 'hsl(229, 57%, 11%)',
+        },
+        bar_gradient: {
+          100: 'hsl(6, 100%, 80%)',
+          200: 'hsl(335, 100%, 65%)',
+        },
+      },
+      fontFamily: {
+        primary: ['Raleway']
+      },
+      borderRadius: {
+        '4xl': '7rem',
+      },
+    },
   },
   plugins: [],
 }
